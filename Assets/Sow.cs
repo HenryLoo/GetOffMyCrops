71c6ee39e72d631b05e8fc330b5e2ece6d1aaf1f
wesley <107821628@qq.com>
2018-09-18 09:50:44 -0700

    first demo commit

---
+++ Assets/Sow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Sow : MonoBehaviour {
+
+    public GameObject player;
+
+    public GameObject seed;
+
+    private Vector3 currentPosition;
+
+    void LateUpdate()
+    {
+       
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            currentPosition = new Vector3(player.transform.position.x, 0.1f, player.transform.position.z);
+
+            Instantiate(seed, currentPosition, new Quaternion(45,0,0,0));
+
+        }
+    }
+}
