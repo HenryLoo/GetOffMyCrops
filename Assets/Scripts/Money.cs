e2fc2f7682e956590a5ae3bc2b659f49f86609e2
Henry Loo <henry_loo@live.ca>
2018-10-12 06:30:50 -0700

    Change some formatting for Money class

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -1,29 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Money
+﻿public class Money
 {
-
+    // The player's current money value
     private static int _currentMoney = 20;
 
+    // Fixed money investment/return constants
     public const int SEED_BUY_PRICE = 5;
-
     public const int CROP_SELL_PRICE = 20;
 
-    public static void Set(int value)
+    // Set the current money value
+    public static void Set( int value )
     {
         _currentMoney = value;
     }
 
-    // add money, or subtract money by set nagetive value; 
-    public static void Add(int value)
+    // Add to the current money value (or subtract by using negative values)
+    public static void Add( int value )
     {
         _currentMoney += value;
-
     }
-
-    // Update is called once per frame
+    
+    // Get the current money value
     public static int Get()
     {
         return _currentMoney;
