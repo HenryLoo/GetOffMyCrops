48885b09373b3fb18881addc52b579398842924e
Henry Loo <henry_loo@live.ca>
2018-11-12 21:55:54 -0800

    Simplify GameSceneSwitcher's scene loading

--- Assets/Scripts/Core/GameSceneSwitcher.cs
+++ Assets/Scripts/Core/GameSceneSwitcher.cs
@@ -34,26 +34,17 @@ public class GameSceneSwitcher
     // Loads a new scene
     public void LoadScene( SCENE_POSITION scene )
     {
-        // Our current design doesn't allow having two scenes loaded at the same time.
-        if( _currentScene != SCENE_POSITION.DUMMY_SCENE )
-        {
-            UnloadScene();
-        }
-
         _currentScene = scene;
         GameInput.DetachInput();
 
+        // In single mode, the current scene is automatically unloaded when a 
+        // new one is loaded
+        SceneManager.LoadSceneAsync( SCENE_LIST[ ( int ) _currentScene ] );
+
         if( _currentScene != SCENE_POSITION.DUMMY_SCENE )
         {
-            Debug.Log( "GameSceneSwitcher.cs loading " + SCENE_LIST[ ( int ) _currentScene ] + " scene" );
+            Debug.Log( "GameSceneSwitcher.LoadScene(): " + 
+                SCENE_LIST[ ( int ) _currentScene ] );
         }
-
-        SceneManager.LoadSceneAsync( SCENE_LIST[ ( int ) _currentScene ] );
-    }
-
-    public void UnloadScene()
-    {
-        SceneManager.UnloadSceneAsync( SCENE_LIST[ ( int ) _currentScene ] );
-        _currentScene = SCENE_POSITION.DUMMY_SCENE;
     }
 }
