236a742e8d0f68b70451eb32da84df8e2fafad20
Henry Loo <henry_loo@live.ca>
2018-12-04 12:23:49 -0800

    Clean up UI code and add level timer sound

--- Assets/Scripts/UI/UIMoneyMeter.cs
+++ Assets/Scripts/UI/UIMoneyMeter.cs
@@ -11,8 +11,8 @@ public class UIMoneyMeter : MonoBehaviour
     public Text ComboBonus;
     private readonly string COMBO_TEXT = "Combo Bonus: +$";
 
-    public Image star;
-    public float goalPosition = 0.7f;
+    public Image Star;
+    private const float GOAL_POSITION = 0.7f;
 
     // Use this for initialization
     void Start()
@@ -28,19 +28,19 @@ public class UIMoneyMeter : MonoBehaviour
 
     public void UpdateMoneyMeter( float currentMoney, float moneyGoal, int combo )
     {
-        
+
         if( currentMoney >= moneyGoal )
         {
             // If current money exceeds the goal, cap the meter at full
-            float fullOver = goalPosition + (1f - goalPosition)*((currentMoney - moneyGoal)/ currentMoney);
+            float fullOver = GOAL_POSITION + ( 1f - GOAL_POSITION ) * ( ( currentMoney - moneyGoal ) / currentMoney );
 
             MoneyValue.value = fullOver;
 
-            if(star.color.a <1)
-            { 
-                Color tempColor = star.color;
-                tempColor.a = 1f;
-                star.color = tempColor;
+            if( Star.color.a < 1 )
+            {
+                Color tempColour = Star.color;
+                tempColour.a = 1f;
+                Star.color = tempColour;
             }
         }
         else if( currentMoney <= 0 )
@@ -50,7 +50,7 @@ public class UIMoneyMeter : MonoBehaviour
         }
         else
         {
-            MoneyValue.value = ( float ) currentMoney / moneyGoal * goalPosition;
+            MoneyValue.value = ( float ) currentMoney / moneyGoal * GOAL_POSITION;
         }
 
         MoneyText.text = "$" + currentMoney + " / $" + moneyGoal;
--- Assets/Scripts/UI/UITimeMeter.cs
+++ Assets/Scripts/UI/UITimeMeter.cs
@@ -8,6 +8,8 @@ public class UITimeMeter : MonoBehaviour
     public Slider TimeValue;
     public Text TimeText;
 
+    private int _previousTime;
+
     // Use this for initialization
     void Start()
     {
@@ -22,31 +24,29 @@ public class UITimeMeter : MonoBehaviour
 
     public void UpdateTimeMeter( float currentTime, float maxTime )
     {
-
-        //if( currentTime >= maxTime ) // no more Slider process bar for time counter
-        //{
-        //    TimeValue.value = 1;
-        //}
-        //if( currentTime <= 0 )
-        //{
-        //    TimeValue.value = 0;
-        //}
-        //else
-        //{
-        //    TimeValue.value = currentTime / maxTime;
-        //}
-
         float timeRemaining = maxTime - currentTime;
         if( timeRemaining < 0 ) timeRemaining = 0;
         string minutes = Mathf.Floor( timeRemaining / 60 ).ToString( "00" );
-        string seconds = (Mathf.Ceil(timeRemaining) % 60 ).ToString( "00" ); // ceil: 0.9-0.1 shows 1, only 0.0 shows 0
+
+        // Ceil: 0.9-0.1 shows 1, only 0.0 shows 0
+        string seconds = ( Mathf.Ceil( timeRemaining ) % 60 ).ToString( "00" );
         TimeText.text = minutes + ":" + seconds;
 
-        if (timeRemaining < 20) // flash number when less then 20 seconds
+        // Flash the timer when less then 20 seconds
+        if( timeRemaining < 20 )
         {
-            float persent = timeRemaining - Mathf.FloorToInt(timeRemaining); // eg 17.6000 - 17 = 0.6 = 60%
+            // Example: 17.6000 - 17 = 0.6 = 60%
+            float percent = timeRemaining - Mathf.FloorToInt( timeRemaining );
             
-            TimeText.rectTransform.localScale = new Vector3(1+ persent, 1 + persent, 1);
+            // Play the countdown sound
+            int currentSeconds = Mathf.RoundToInt( timeRemaining );
+            if( _previousTime > currentSeconds )
+            {
+                SoundController.PlaySound( SoundType.CountdownBlip, false );
+            }
+            _previousTime = currentSeconds;
+
+            TimeText.rectTransform.localScale = new Vector3( 1 + percent, 1 + percent, 1 );
 
             //Debug.Log(seconds + "time left: " + timeRemaining + " persent " + TimeText.rectTransform.localScale.x);
         }
